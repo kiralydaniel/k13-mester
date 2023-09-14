3921a09b33dd81e1abc635eb055d227d83f71fe3
kiralydaniel <kiralydani93@gmail.com>
2023-09-14 22:34:11 +0200

    Add Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Kezdőlap</nuxt-link></li>
+        <li><nuxt-link to="/kreak2">Rólunk</nuxt-link></li>
+        <li><a href="#">Árajánlat</a></li>
+        <li><a href="#">Kapcsolat</a></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <style scoped>
+  @import '../assets/Style.css';
+  </style>
+  
+  
